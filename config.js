a9c5fa939817bc15197926fe81960542d634ac42
bleu rainz <ryanchirwa28@gmail.com>
2024-12-20 14:36:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "uNRVTAhB#wOMxDzpMf_mUcBra3i0_nXEAfDA7Y3LsjhKwlRo8etc",
+    SESSION_ID: process.env.SESSION_ID || "LQA0VRCI#LhXUX0gRCYHmmi2ZltCjwcquYomYnkOUB70eu1OXUtQ",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
     MODE: process.env.MODE || "private",
     AUTO_VOICE: process.env.AUTO_VOICE || "false",
     AUTO_STICKER: process.env.AUTO_STICKER || "false",
     AUTO_REPLY: process.env.AUTO_REPLY || "false",
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/nqCsIHZ.jpeg",
+    ALIVE_IMG: process.env.ALIVE_IMG || "false",
     ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M 𝘽𝙇𝙀𝙐 𝙍𝘼𝙄𝙉𝙕_MD WHATSAPP BOT 😊♻️",
     ANTI_LINK: process.env.ANTI_LINK || "true",
     ANTI_BAD: process.env.ANTI_BAD || "true",
@@ -21,7 +21,7 @@ module.exports = {
     FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
     AUTO_REACT: process.env.AUTO_REACT || "false",
     HEART_REACT: process.env.HEART_REACT || "true",
-    OWNER_REACT: process.env.OWNER_REACT || "false",
-    BOT_NAME: process.env.BOT_NAME || "0786313048",
+    OWNER_REACT: process.env.OWNER_REACT || "true",
+    BOT_NAME: process.env.BOT_NAME || "bleu rainz",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
